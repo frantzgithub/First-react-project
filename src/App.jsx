007b7643585026441df24ca560f80c56e28d2a7e
lin <139031054+frantzgithub@users.noreply.github.com>
2023-11-28 18:22:32 -0400

    react project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+export default function App() {
+
+  return (
+      <div>
+        <img src="/react.png" alt="react logo" />
+        <h1>I learn react</h1>
+        <p>React is amazing</p>
+        <div>
+          <button>My button</button>
+          <button>I love react</button>
+        </div>
+        <hr />
+        <h2>Do you like react?</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took </p>
+      </div>
+  )
+}
